21292e2d5e964c70e8abaa5468824e1b1ad025bd
gagan1146 <gagansharma99690@gmail.com>
2025-02-05 21:42:46 +0530

    Second Commit By adding functionality of login popup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import Home from './pages/Home/Home'
-import {Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
+import LoginPopup from './components/LoginPopup/LoginPopup'
 
 const App = () => {
+
+  const [showLogin, setShowLogin] = useState(false)
+
   return (
-    <div className='app'>
-      <Navbar/>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/cart' element={<Cart/>}/>
-        <Route path='/place' element={<PlaceOrder/>}/>
-      </Routes>
-      <Footer/>
-    </div>
+    <>
+      {showLogin ? <LoginPopup setShowLogin={setShowLogin} /> : <></>}
+      <div className='app'>
+        <Navbar setShowLogin={setShowLogin} />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/place' element={<PlaceOrder />} />
+        </Routes>
+        <Footer />
+      </div>
+    </>
   )
 }
 
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,17 +1,18 @@
 import React, { useState } from 'react'
 import './Navbar.css'
 import {assets} from '../../assets/frontend_assets/assets'
+import {Link} from 'react-router-dom'
 
-const Navbar = () => {
+const Navbar = ({setShowLogin}) => {
     const [menu,setMenu] = useState("");
   return (
     <div className='navbar'>
       <img src={assets.logo} alt="" />
       <ul className="navbar-menu">
-        <li onClick={()=>{setMenu("home")}} className={menu=="home"?"active":""}>Home</li>
-        <li onClick={()=>{setMenu("menu")}} className={menu=="menu"?"active":""}>Menu</li>
-        <li onClick={()=>{setMenu("mobile-app")}} className={menu=="mobile-app"?"active":""}>Mobile-app</li>
-        <li onClick={()=>{setMenu("contact-us")}} className={menu=="contact-us"?"active":""}>Contact Us</li>
+        <Link to='/' onClick={()=>{setMenu("home")}} className={menu=="home"?"active":""}>Home</Link>
+        <a href='#explore-menu' onClick={()=>{setMenu("menu")}} className={menu=="menu"?"active":""}>Menu</a>
+        <a href='#app-download' onClick={()=>{setMenu("mobile-app")}} className={menu=="mobile-app"?"active":""}>Mobile-app</a>
+        <a href='#footer' onClick={()=>{setMenu("contact-us")}} className={menu=="contact-us"?"active":""}>Contact Us</a>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
@@ -19,7 +20,7 @@ const Navbar = () => {
             <img src={assets.basket_icon} alt="" />
           <div className="dot"></div>
         </div>
-        <button>Sign In</button>
+        <button onClick={()=>setShowLogin(true)}>Sign In</button>
       </div>
 
     </div>
