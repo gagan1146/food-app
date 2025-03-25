67785b079ba984e850391c6eec1eae23315ccf15
gagan1146 <gagansharma99690@gmail.com>
2025-03-25 12:55:15 +0530

    commit1

--- backend/config/db.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose'
-import dotenv from 'dotenv';
-dotenv.config()
-
-export const ConnectDB = async ()=>{
-    await mongoose.connect(process.env.mongo_uri)
-    .then(()=>{console.log("DB Connected")});
-}
-
