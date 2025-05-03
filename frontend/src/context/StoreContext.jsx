06d87bafa79dd00ad6770052e1fd30d7c83b447e
gagan1146 <158063668+gagan1146@users.noreply.github.com>
2025-05-03 14:02:57 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -7,7 +7,7 @@ const StoreContextProvider = (props) => {
 
     const [cartItems,setCartItems] = useState({});
 
-    const url = "http://localhost:4000"
+    const url = "https://food-app-backend-taae.onrender.com"
     const [token, setToken] = useState("");
     const [food_list, setFood_list] = useState([])
 
