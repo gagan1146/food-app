2f6c18313eec7c9ed9bc2ff99debf7780f705d4b
gagan1146 <gagansharma99690@gmail.com>
2025-05-03 13:57:35 +0530

    Updating backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://gagansharma99690:asdf@cluster0.ldwzn.mongodb.net/food-del').then(()=>console.log("DB connected")).catch(()=>console.log("Not Connected"));
+    await mongoose.connect(process.env.URI).then(()=>console.log("DB connected")).catch(()=>console.log("Not Connected"));
 }
